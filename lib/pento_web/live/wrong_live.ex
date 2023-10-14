9d265e1204f78e334e8a82750eb8f993c2e84883
G M T Borges <contact@gmtborges.com>
2023-10-14 15:14:06 -0300

    wrong_live route

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,8 +1,22 @@
 defmodule PentoWeb.WrongLive do
-  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, :live}
+  use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, score: 0, message: "Make a guess:")}
+  end
+
+  def handle_event("guess", %{"number" => guess} = _data, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again. "
+    score = socket.assigns.score - 1
+
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score
+      )
+    }
   end
 
   def render(assigns) do
