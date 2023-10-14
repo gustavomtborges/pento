76e818f9465752155357863a123a1043dd33f437
G M T Borges <contact@gmtborges.com>
2023-10-14 16:37:31 -0300

    finish guess game

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,19 +2,39 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       time: time(),
+       number: Enum.random(1..10),
+       is_right?: false
+     )}
   end
 
   def handle_event("guess", %{"number" => guess} = _data, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
+    is_right? = check_guess(socket.assigns.number, String.to_integer(guess))
+    dbg(is_right?)
+
+    message =
+      case(is_right?) do
+        true -> "Your guess: #{guess}. Right!"
+        false -> "Your guess: #{guess}. Wrong. Guess again. "
+      end
+
+    score =
+      case(is_right?) do
+        true -> socket.assigns.score + 1
+        false -> socket.assigns.score - 1
+      end
 
     {
       :noreply,
       assign(
         socket,
         message: message,
-        score: score
+        score: score,
+        is_right?: is_right?
       )
     }
   end
@@ -30,6 +50,17 @@ defmodule PentoWeb.WrongLive do
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
       <% end %>
     </h2>
+    <%= if @is_right? do %>
+    <.link href="/guess">Restart</.link>
+    <% end %>
     """
+  end
+
+  defp check_guess(number, guess) do
+    number == guess
+  end
+
+  defp time do
+    DateTime.utc_now() |> to_string
   end
 end
